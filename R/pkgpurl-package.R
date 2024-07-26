fb4652a58b9d96c4884abfc17c1d5ddd471f42aa
Salim B <git@salim.space>
2024-07-26 15:49:09 +0200

    style: name arg

--- R/pkgpurl-package.R
+++ R/pkgpurl-package.R
@@ -1,6 +1,6 @@
 #' @section Package configuration:
 #'
-#' `r pkgsnip::md_snip("pkg_config", pkg = this_pkg)`
+#' `r pkgsnip::md_snip(id = "pkg_config", pkg = this_pkg)`
 #'
 #' ```{r, echo = FALSE}
 #' pal::print_pkg_config(this_pkg)
